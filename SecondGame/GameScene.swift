4a62c95af1672b04bcb234118f71202e66e5596d
JohnVanGilder <john.vangilder.1@gmail.com>
2016-10-14 16:56:10 -0500

    Replacing all files with current versions
    Because XCode stopped recognizing this as a repo, for some reason

--- SecondGame/GameScene.swift
+++ SecondGame/GameScene.swift
@@ -61,13 +61,14 @@ class playerSpriteNode: SKSpriteNode{
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
         let player = playerSpriteNode(imageNamed: "Spaceship")
-        let loser = SKLabelNode()
-    
-    
+        let scoreboard = SKLabelNode()
+        let shieldbar = SKSpriteNode(imageNamed: "shield4")
+        var score = 0
     
 //-------------------------------------------------------------------------------------------------
     func addPlayer(){
         //add the player to the scene
+
 
         player.physicsBody = SKPhysicsBody (rectangleOf: player.size)
         player.physicsBody?.isDynamic = true
@@ -78,8 +79,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //Define the player's position
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-        //add player to the scene
+        player.zPosition = 100
+        //add player to the scene after ensuring it has the right sprite texture
+        player.run(SKAction.setTexture(SKTexture(imageNamed: "Spaceship")))
         addChild(player)
+        
+        //create and add the shield bar to the scene
+        shieldbar.texture = SKTexture(imageNamed: "shield4")
+        shieldbar.position = CGPoint(x: shieldbar.size.width , y: shieldbar.size.height + 5)
+        shieldbar.zPosition = -1
+        addChild(shieldbar)
+        
+        //create and add the scoreboard to the scene
+        scoreboard.text = "Score: 000"
+        scoreboard.position = CGPoint(x: size.width * 0.9 - scoreboard.frame.size.width/2, y: size.height * 0.9)
+        
+        scoreboard.fontColor = UIColor.red
+
+        addChild(scoreboard)
+        
+        
     }
     
 //-------------------------------------------------------------------------------------------------
@@ -162,10 +181,39 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let actionMoveDone = SKAction.removeFromParent()
         
         
-        
-        
+
         asteroid.run(SKAction.sequence([actionMove, actionMoveDone]))
         
+        
+    }
+    
+    func shoot(){
+        let laser = SKSpriteNode(imageNamed: "laser")
+        //set sprite location
+        laser.position = player.position
+        
+        //makes sure it's behind the ship (all others have default 0 for zposition)
+        laser.zPosition = -1
+        
+        laser.physicsBody = SKPhysicsBody(rectangleOf: laser.size)
+        laser.physicsBody?.isDynamic = true
+        laser.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        laser.physicsBody?.affectedByGravity = false
+        laser.physicsBody?.contactTestBitMask = PhysicsCategory.Asteroid
+        laser.physicsBody?.collisionBitMask = PhysicsCategory.None
+        
+        
+        
+        
+        
+        addChild(laser)
+        
+        let actionMove = SKAction.move(to: CGPoint(x: size.width + laser.size.width/2, y:player.position.y), duration: 1.0)
+        let actionDone = SKAction.removeFromParent()
+        
+        laser.run(SKAction.sequence([actionMove, actionDone]))
+        
+
         
     }
 //-------------------------------------------------------------------------------------------------
@@ -180,30 +228,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             //create sprite
 
             if(player.lives > 0){
-                let laser = SKSpriteNode(imageNamed: "laser")
-                //set sprite location
-                laser.position = player.position
-            
-                //makes sure it's behind the ship (all others have default 0 for zposition)
-                laser.zPosition = -1
-            
-                laser.physicsBody = SKPhysicsBody(rectangleOf: laser.size)
-                laser.physicsBody?.isDynamic = true
-                laser.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
-                laser.physicsBody?.affectedByGravity = false
-                laser.physicsBody?.contactTestBitMask = PhysicsCategory.Asteroid
-                laser.physicsBody?.collisionBitMask = PhysicsCategory.None
-            
-            
-            
-            
-            
-                addChild(laser)
-            
-                let actionMove = SKAction.move(to: CGPoint(x: size.width + laser.size.width/2, y:player.position.y), duration: 1.0)
-                let actionDone = SKAction.removeFromParent()
-            
-                laser.run(SKAction.sequence([actionMove, actionDone]))
+                shoot()
+               
             }else {
                 player.lives = 5
                 sleep(UInt32(1.0))
@@ -215,54 +241,57 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        var touchPosition :CGPoint = touches.first!.location(in: view)
+        if(player.lives > 0){
+            var touchPosition :CGPoint = touches.first!.location(in: view)
         
-        if(touchPosition.x < size.width/3){
+            if(touchPosition.x < size.width/3){
             
-            touchPosition = (scene?.convertPoint(fromView: touchPosition))!
-            if(touchPosition.x < player.size.width/2){
-                touchPosition.x += player.size.width/2
+                touchPosition = (scene?.convertPoint(fromView: touchPosition))!
+                if(touchPosition.x < player.size.width/2){
+                    touchPosition.x += player.size.width/2
+                }
+                if(touchPosition.y < player.size.height/2){
+                    touchPosition.y += player.size.height/2
+                }else if(touchPosition.y > size.height - player.size.height/2){
+                    touchPosition.y -= player.size.height/2
+                }
+                
+                let length = abs(CGFloat(hypotf(Float(touchPosition.x - player.position.x), Float(touchPosition.y - player.position.y))))
+                let speed = 500.0
+            
+                //offset player sprite from the user's finger so he can see the ship
+                player.run(SKAction.move(to: CGPoint(x: touchPosition.x + 30, y: touchPosition.y), duration: Double(length)/speed))
             }
-            if(touchPosition.y < player.size.height/2){
-                touchPosition.y += player.size.height/2
-            }else if(touchPosition.y > size.height - player.size.height/2){
-                touchPosition.y -= player.size.height/2
-            }
-            
-            let length = abs(CGFloat(hypotf(Float(touchPosition.x - player.position.x), Float(touchPosition.y - player.position.y))))
-            let speed = 500.0
-            
-            player.run(SKAction.move(to: CGPoint(x: touchPosition.x, y: touchPosition.y), duration: Double(length)/speed))
-            
         }
 
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        if(player.lives > 0){
             var touchPosition :CGPoint = touches.first!.location(in: view)
         
             if(touchPosition.x < size.width/3){
         
-            touchPosition = (scene?.convertPoint(fromView: touchPosition))!
-            if(touchPosition.x < player.size.width/2){
+                touchPosition = (scene?.convertPoint(fromView: touchPosition))!
+                if(touchPosition.x < player.size.width/2){
                 touchPosition.x += player.size.width/2
-            }
-            if(touchPosition.y < player.size.height/2){
-                touchPosition.y += player.size.height/2
-            }else if(touchPosition.y > size.height - player.size.height/2){
-                touchPosition.y -= player.size.height/2
-            }
+                }
+                if(touchPosition.y < player.size.height/2){
+                    touchPosition.y += player.size.height/2
+                }else if(touchPosition.y > size.height - player.size.height/2){
+                    touchPosition.y -= player.size.height/2
+                }
             
-            let length = abs(CGFloat(hypotf(Float(touchPosition.x - player.position.x), Float(touchPosition.y - player.position.y))))
-            let speed = 500.0
+                let length = abs(CGFloat(hypotf(Float(touchPosition.x - player.position.x), Float(touchPosition.y - player.position.y))))
+                let speed = 500.0
             
-            player.run(SKAction.move(to: CGPoint(x: touchPosition.x, y: touchPosition.y), duration: Double(length)/speed))
+            //offset player sprite from user's finger so she can see the ship
+            player.run(SKAction.move(to: CGPoint(x: touchPosition.x + 30, y: touchPosition.y), duration: Double(length)/speed))
+            
+            }
+        
             
         }
-        
-            
-        
 
     }
     
@@ -270,6 +299,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //set collision behavior for asteroids
     func projectileDidCollideWithAsteroid(asteroid: SKSpriteNode, projectile: SKSpriteNode){
         print("hit")
+    
         projectile.removeFromParent()
         
         let texture1 = SKTexture(imageNamed:"explosion")
@@ -277,21 +307,50 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         asteroid.texture = texture1
        
         asteroid.run(SKAction.sequence([SKAction.move(to: asteroid.position, duration: 0), SKAction.removeFromParent()]))
+        
+        score(interval: 100)
 
-    
     
     }
 
 //-------------------------------------------------------------------------------------------------
+//sets scoring behavior
+    func score(interval: Int){
+        score += interval
+        scoreboard.text = "Score: \(score)"
+        
+        scoreboard.position = CGPoint(x: size.width * 0.9 - scoreboard.frame.size.width/2, y: size.height * 0.9)
+        //if(score % 1000 == 0 && player.lives < 5){
+        //player.lives += 1
+        //}
+        
+    }
+    
+//-------------------------------------------------------------------------------------------------
+    
 //set collision behavior for ship
     func asteroidDidCollideWithPlayer(asteroid: SKSpriteNode, player:playerSpriteNode){
         print("rip")
         
         let texture1 = SKTexture(imageNamed:"explosion")
         asteroid.texture = texture1
+
+        if(player.lives > 1){
+            player.run(SKAction.sequence([SKAction.setTexture(SKTexture(imageNamed: "shieldship")), SKAction.wait(forDuration: 0.5), SKAction.setTexture(SKTexture(imageNamed: "Spaceship"))]))
+        }
+        
         asteroid.run(SKAction.sequence([SKAction.move(to: asteroid.position, duration: 0), SKAction.removeFromParent()]))
         player.lives -= 1
-        if(player.lives == 0){
+        
+        if(player.lives == 4){
+            shieldbar.texture = SKTexture(imageNamed: "shield3")
+        }else if(player.lives == 3){
+            shieldbar.texture = SKTexture(imageNamed: "shield2")
+        }else if(player.lives == 2){
+            shieldbar.texture = SKTexture(imageNamed: "shield1")
+        }else if(player.lives == 1){
+            shieldbar.removeFromParent()
+        }else if(player.lives == 0){
             gameOver();
         }
         
@@ -325,8 +384,44 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
 //-------------------------------------------------------------------------------------------------
     func gameOver(){
-        player.removeFromParent()
 
+        score = 0
+        
+        //Sets up the animation to break up the ship and explode
+        let ship1 = SKSpriteNode(imageNamed: "spaceshipTopLeft")
+        let ship2 = SKSpriteNode(imageNamed: "spaceshipTopRight")
+        let ship3 = SKSpriteNode(imageNamed: "spaceshipBotLeft")
+        let ship4 = SKSpriteNode(imageNamed: "spaceshipBotRight")
+
+        //spawns the ship parts at the right place
+        ship1.position = player.position
+        ship2.position = CGPoint(x: player.position.x - 6, y: player.position.y - 1)
+        ship3.position = CGPoint(x: player.position.x, y: player.position.y + 1)
+        ship4.position = CGPoint(x: player.position.x - 1, y: player.position.y)
+        
+
+        addChild(ship1)
+        addChild(ship2)
+        addChild(ship3)
+        addChild(ship4)
+        
+        //blows up the ship after making sure it's not moving
+        player.removeAllActions()
+        player.removeFromParent()
+        let explosion = SKSpriteNode(imageNamed: "explosion")
+        explosion.position = player.position
+        addChild(explosion)
+        explosion.run(SKAction.sequence([SKAction.setTexture(SKTexture(imageNamed: "explosion")),SKAction.scale(to: 0.2, duration: 0), SKAction.scale(by: 5, duration: 2), SKAction.removeFromParent()]))
+
+        //plays the animation of the ship breaking up and exploding
+        ship1.run(SKAction.sequence([SKAction.move(by: CGVector(dx: random(min:20, max: 100), dy: random(min:20, max: 100)), duration: 4), SKAction.setTexture(SKTexture(imageNamed: "explosion")), SKAction.wait(forDuration: 0.2), SKAction.removeFromParent()]))
+        ship2.run(SKAction.sequence([SKAction.move(by: CGVector(dx: -1 * random(min:20, max: 100), dy: random(min:20, max: 100)), duration: 3), SKAction.setTexture(SKTexture(imageNamed: "explosion")), SKAction.wait(forDuration: 0.2), SKAction.removeFromParent()]))
+        ship3.run(SKAction.sequence([SKAction.move(by: CGVector(dx: -1 * random(min:20, max: 100), dy: -1 * random(min:20, max: 100)), duration: 3.5), SKAction.setTexture(SKTexture(imageNamed: "explosion")), SKAction.wait(forDuration: 0.2), SKAction.removeFromParent()]))
+        ship4.run(SKAction.sequence([SKAction.move(by: CGVector(dx: random(min:20, max: 100), dy: -1 * random(min:20, max: 100)), duration: 3), SKAction.setTexture(SKTexture(imageNamed: "explosion")), SKAction.wait(forDuration: 0.2), SKAction.removeFromParent()]))
+        
+
+        
+        let loser = SKLabelNode()
         loser.text = "GAME OVER"
         loser.fontSize = 75
         loser.fontColor = SKColor.red
